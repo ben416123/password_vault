1a8e8e96ec4de0df70a22a9f32b18d069255a277
ben416123 <c3045540@newcastle.ac.uk>
2024-06-28 18:40:45 +0100

    Initial password check function

--- vault.py
+++ vault.py
@@ -5,6 +5,7 @@ window = Tk()
 
 window.title("Password Vault")
 
+
 def loginScreen():
     window.geometry("250x100")
 
@@ -12,10 +13,23 @@ def loginScreen():
     label.config(anchor=CENTER)
     label.pack()
 
-    txt = Entry(window, width=10)
+    def checkPassword():
+        password = "Test"
+
+        if password == txt.get():
+            print("Correct")
+        else:
+            label1.config(text="Wrong")
+
+    txt = Entry(window, width=20, show="*")
     txt.pack()
-    button = Button(window, text="Submit")
-    button.pack(pady=5)
+    txt.focus()
+
+    label1 = Label(window)
+    label1.pack()
+
+    button = Button(window, text="Submit", command=checkPassword)
+    button.pack(pady=10)
 
 
 loginScreen()
